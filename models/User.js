2d940aa105b87ace76068e0e96e6385eca7ada08
Abraxia Dizarkian <dizarkian@outlook.com>
2026-02-24 02:29:43 -0800

    Added role field to user model

--- models/User.js
+++ models/User.js
@@ -14,6 +14,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ["student", "trainer", "employer", "admin"],
+    default: "student"
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
